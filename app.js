8e5d52ed6a96d0d74a776aa1ee9e2cf4d2c61bd8
alexx1701 <92756383+alexx1701@users.noreply.github.com>
2022-02-10 19:37:00 -0600

    finished complete/remove

--- app.js
+++ app.js
@@ -38,7 +38,17 @@ const deleteCheck = (e) => {
     //delete todo
     if (item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add('fall');
+        //removes the element after the transition has ended
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+    //check todo
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 }
 
